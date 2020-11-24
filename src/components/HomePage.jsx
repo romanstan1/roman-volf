6908a387013208a080c99eb6efb8220448323661
Roman Stankiewicz <roman@neverbland.com>
2020-11-24 18:25:28 +0000

    Adds assets

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,65 @@
 import React from "react";
+import styled, { css } from "styled-components";
 import Section from "./Section";
 import { H1 } from "./Text";
+
+import VO3 from "../assets/vividorange-3.JPG";
+import VO4 from "../assets/vividorange-4.JPG";
+
+
+const CarouselWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-grow: 0;
+  height: 500px;
+  width: 100%; 
+  img {
+    margin: 20px;
+  }
+`;
+
+const Landscape = styled.img`
+  width:${(props => props.width ? props.width : 400)}px;
+  /* height: auto; */
+`;
+
+const Portrait = styled.img`
+  /* width: auto; */
+  height:${(props => props.height ? props.height : 400)}px;
+ 
+`;
+
+
+const Carousel = ({ images }) => {
+  return (
+    <CarouselWrapper>
+      {images.map(item => {
+        if(item.size === "portrait") {
+          return <Portrait src={item.image} height={item.height} key={item.image} alt="" />;
+        }
+        return <Landscape src={item.image} width={item.width} key={item.image} alt="" />;
+      })}
+    </CarouselWrapper>
+  );
+};
+
+const vividOrange = [
+  { 
+    image: VO3, 
+    size:"landscape", 
+    // width: 500,
+  },
+  { 
+    image: VO4, 
+    size:"portrait", 
+  }
+];
 
 const Home = () => {
   return (
     <Section minHeight>
-      <H1>Home page</H1>
+      <Carousel images={vividOrange} /> 
     </Section>
   );
 };
--- src/components/StatementPage.jsx
+++ src/components/StatementPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import Section from "./Section";
-import { H1 } from "./Text";
+import { H1, P1 } from "./Text";
+
+const Para = styled(P1)`
+  margin: 150px 7vw 50px 7vw;
+  line-height: 2rem;
+`;
 
 const StatementPage = () => {
   return (
     <Section minHeight>
       <H1>Statement Page</H1>
+      <Para>
+        Hi everyone and thank you for taking time out of your day to take a look at how I spend mine. I am a designer/artist person with a over 20 years experience under my belt. I’ve worked on ads for Nissan, posters for movies, reports for lawyers and logos for businesses. I’m always working on something new.
+        If you are interested in hiring me for freelance design work please feel free to email me. I’m always excited to talk about new potential design projects.
+        If you are interested in staying up to date with new poster releases please sign up for my mailing list or check out any of my social media outlets.
+      </Para>
     </Section>
   );
 };
